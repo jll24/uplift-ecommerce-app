4ecb77bf01461b5adc693318b14de7d30016046d
Lester Lacson <iamjohnlacson@gmail.com>
2021-12-24 14:14:05 +0800

    Fixed: path

--- backend/index.js
+++ backend/index.js
@@ -33,10 +33,10 @@ app.use('/api/checkout', stripeRoute)
 
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('./frontend/build'));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, './frontend', 'build', 'index.html'));
     });
   }
 
