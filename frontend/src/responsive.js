a633b256382485274f6efa7d458fb520eed4f6ed
Lester Lacson <iamjohnlacson@gmail.com>
2021-12-29 09:11:20 +0800

    Increased max-width for mobile ver

--- frontend/src/responsive.js
+++ frontend/src/responsive.js
@@ -4,7 +4,7 @@ import {css} from 'styled-components'
 
 export const mobile = (props) => {
     return css`
-    @media only screen and (max-width: 376px) {
+    @media only screen and (max-width: 476px) {
         ${props}
     }`;
 }
