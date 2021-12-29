208c970dac293064db7f628ac125d96254a597bd
Lester Lacson <iamjohnlacson@gmail.com>
2021-12-29 08:38:03 +0800

    Updates

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ app.use('/api/checkout', stripeRoute)
       res.sendFile(path.resolve(__dirname, "frontend","build","index.html"))
     })
   }
-
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
      console.log(`Our app is running on port ${ PORT }`);
