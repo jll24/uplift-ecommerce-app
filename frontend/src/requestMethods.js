cf26ae71ba146b2452191b8849ac471d5dccf39d
Lester Lacson <iamjohnlacson@gmail.com>
2021-12-29 10:09:27 +0800

    Changed base url

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = "https://hi-wrld.herokuapp.com/"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOTc3OWU3YTMxMGY4ZTI2NGI1NjI2NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzczNTY5NywiZXhwIjoxNjM3OTk0ODk3fQ.PDVMV_Z7MAFN2IkkQf-7jxhbbzAfPhDNDbIYAIfOeMA"
 
 export const publicRequest = axios.create({
